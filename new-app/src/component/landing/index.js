66f3ef5ecbcd8065d2f977d37ef9dbf3b0f05032
Aaliyah Farah Adibah <73101444+aaliyahfarah@users.noreply.github.com>
2024-12-31 13:08:52 +0700

    add product

--- new-app/src/component/landing/index.js
+++ new-app/src/component/landing/index.js
@@ -1,50 +1,47 @@
+import styles from './index.css';
+import { fill } from "@cloudinary/url-gen/actions/resize";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 let Landing = () => {
     return (
         <div>
-            <nav class="navbar navbar-light bg-light navbar-expand-lg ">
-                <div class="container-fluid">
-                    {/* <a class="navbar-brand" href="#">Navbar</a> */}
-                    <img src="image.png" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+            <nav className="navbar navbar-light bg-light navbar-expand-lg ">
+                <div className="container-fluid">
+                    {/* <a className="navbar-brand" href="#">Navbar</a> */}
+                    <img src="image.png" alt="" width="30" height="24" className="d-inline-block align-text-top" />
                     {/* Bliken */}
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Product</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Product</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About us</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About us</a>
                             </li>
                         </ul>
-                        <form class="d-flex">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                            <button class="btn btn-light btn-outline-success" type="submit">Search</button>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                            <button className="btn btn-primary text-white" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
             </nav>
 
             <div>
-                <div class="card text-bg-dark">
-                    <img src="motor.png" class="card-img" alt="..." />
-                    <div class="card-img-overlay">
-                        {/* <h5 class="card-title">NEW PRODUCT is here!</h5>
-                        <p class="card-text">Get this product before sold!</p>
-                        <button class="btn btn-light btn-outline-success" type="submit">Shop now!</button> */}
-                    </div>
-                </div>
+                <BannerSection />
                 <div className="container">
-                    <h2>On Sale! Don't miss it</h2>
-                    <SaleCard />
+                    {/* <SaleCard /> */}
                     {/* sale */}
-                    <h2>New Products!</h2>
-                    <NewCard />
-                    <h1>hello world </h1>
+                    {/* <h2>New Products!</h2> */}
+                    {/* <NewCard /> */}
+                    <ProductCard />
                 </div>
 
             </div>
@@ -64,70 +61,87 @@ let Landing = () => {
     )
 }
 
+let BannerSection = () => {
+    return (
+        <div className="banner position-relative">
+            <img
+                src="motor.png" alt="Banner Background" className="img-fluid w-100"
+            />
+            <div className="banner-text position-absolute top-50 start-50 translate-middle text-center text-white">
+                <h2 className="display-3 fw-bold">The worldwide best seller product is here</h2>
+                <p className="lead">Limited stock! Buy BMW 430I XDRIVE before sold out</p>
+                {/* #newproduct */}
+                <a href="#" className="btn btn-primary btn-lg mt-3">
+                    Discover new product
+                </a>
+            </div>
+        </div>
+    );
+}
+
 let SaleCard = () => {
     return (
-        <div className="container mt-4">
-            <div className="d-flex overflow-auto card-group">
-                <div class="row row-cols-1 row-cols-md-3 g-4">
-                    <div class="col">
-                        <div className="card h-100" style={{ width: "200px" }}>
-                            <img src="vespa.png" class="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title">VESPA LX 125</h5>
-                                <p className="card-text"> Some quick example text. </p>
-                                <button class="btn btn-primary" type="submit">Buy</button>
-                            </div>
+        <div className="container mt-5 mb-5 text-center">
+            <h2 className=" mb-4">On Sale! Don't miss it</h2>
+            <div className="row  g-4">
+                <div className="col">
+                    <div className="card card-sale " >
+                        <img src="vespa.png" className="card-img-top" alt="..." />
+                        <div className="card-body mb-2">
+                            <h5 className="card-title">VESPA LX 125</h5>
+                            <p className="card-text"> Some quick example text. </p>
                         </div>
+                        <button className="btn btn-primary m-2" type="submit">Buy</button>
                     </div>
-                    <div class="col">
-                        <div className="card h-100" style={{ width: "200px" }}>
-                            <img src="vespa2.png" class="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title">VESPA SPRINT 150 ABS</h5>
-                                <p class="card-text">The sporty elegence for your bold city lifestyle</p>
-                                <button class="btn btn-primary" type="submit">Buy</button>
-                            </div>
+                </div>
+                <div className="col">
+                    <div className="card card-sale">
+                        <img src="vespa2.png" className="card-img-top" alt="..." />
+                        <div className="card-body mb-2">
+                            <h5 className="card-title">VESPA SPRINT 150 ABS</h5>
+                            <p className="card-text">The sporty elegence for your bold city lifestyle</p>
                         </div>
+                        <button className="btn btn-primary m-2" type="submit">Buy</button>
                     </div>
-                    <div class="col">
-                        <div className="card h-100" style={{ width: "200px" }}>
-                            <img src="vespa3.png" class="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title">Vespa 946 Dragon</h5>
-                                <p className="card-text">A true masterpiece of Italian manufacturing, the Vespa 946 Dragon is a collector's item, a limited edition numbered series of 1888</p>
-                                <button class="btn btn-primary" type="submit">Buy</button>
-                            </div>
+                </div>
+                <div className="col">
+                    <div className="card card-sale">
+                        <img src="vespa3.png" className="card-img-top" alt="..." />
+                        <div className="card-body mb-2">
+                            <h5 className="card-title">Vespa 946 Dragon</h5>
+                            <p className="card-text">A true masterpiece of Italian manufacturing, the Vespa 946 Dragon is a collector's item, a limited edition numbered series of 1888</p>
                         </div>
+                        <button className="btn btn-primary m-2" type="submit">Buy</button>
                     </div>
-                    <div class="col">
-                        <div className="card h-100" style={{ width: "200px" }}>
-                            <img src="vespa2.png" class="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title">VESPA SPRINT 150 ABS</h5>
-                                <p class="card-text">The sporty elegence for your bold city lifestyle</p>
-                                <button class="btn btn-primary" type="submit">Buy</button>
-                            </div>
+                </div>
+                <div className="col">
+                    <div className="card card-sale">
+                        <img src="vespa2.png" className="card-img-top" alt="..." />
+                        <div className="card-body mb-2 ">
+                            <h5 className="card-title">VESPA SPRINT 150 ABS</h5>
+                            <p className="card-text">The sporty elegence for your bold city lifestyle</p>
                         </div>
+                        <button className="btn btn-primary m-2" type="submit">Buy</button>
                     </div>
-                    <div class="col">
-                        <div className="card h-100" style={{ width: "200px" }}>
-                            <img src="vespa.png" class="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title">VESPA LX 125</h5>
-                                <p className="card-text">Some quick example text.</p>
-                                <button class="btn btn-primary" type="submit">Buy</button>
-                            </div>
+                </div>
+                <div className="col">
+                    <div className="card card-sale">
+                        <img src="vespa.png" className="card-img-top" alt="..." />
+                        <div className="card-body mb-2">
+                            <h5 className="card-title">VESPA LX 125</h5>
+                            <p className="card-text">Some quick example text.</p>
                         </div>
+                        <button className="btn btn-primary m-2" type="submit">Buy</button>
                     </div>
-                    <div class="col">
-                        <div className="card h-100" style={{ width: "200px" }}>
-                            <img src="vespa3.png" class="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title">Vespa 946 Dragon</h5>
-                                <p className="card-text">A true masterpiece of Italian manufacturing, the Vespa 946 Dragon is a collector's item, a limited edition numbered series of 1888</p>
-                                <button class="btn btn-primary" type="submit">Buy</button>
-                            </div>
+                </div>
+                <div className="col">
+                    <div className="card card-sale">
+                        <img src="vespa3.png" className="card-img-top" alt="..." />
+                        <div className="card-body mb-2">
+                            <h5 className="card-title">Vespa 946 Dragon</h5>
+                            <p className="card-text">A true masterpiece of Italian manufacturing, the Vespa 946 Dragon is a collector's item, a limited edition numbered series of 1888</p>
                         </div>
+                        <button className="btn btn-primary m-2" type="submit">Buy</button>
                     </div>
                 </div>
             </div>
@@ -139,54 +153,54 @@ let NewCard = () => {
     return (
         <div className="container mt-4">
             <div className="d-flex overflow-auto card-group">
-                <div class="row row-cols-1 row-cols-md-3 g-4">
-                    <div class="col">
+                <div className="row row-cols-1 row-cols-md-3 g-4">
+                    <div className="col">
                         <div className="card h-100" style={{ width: "200px" }}>
-                            <img src="vespa.png" class="card-img-top" alt="..."/>
+                            <img src="vespa.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">VESPA LX 125</h5>
                                 <p className="card-text"> Some quick example text. </p>
-                                <button class="btn btn-primary" type="submit">Buy</button>
+                                <button className="btn btn-primary" type="submit">Buy</button>
                             </div>
                         </div>
                     </div>
-                    <div class="col">
+                    <div className="col">
                         <div className="card h-100" style={{ width: "200px" }}>
-                            <img src="vespa2.png" class="card-img-top" alt="..."/>
+                            <img src="vespa2.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">VESPA SPRINT 150 ABS</h5>
-                                <p class="card-text">The sporty elegence for your bold city lifestyle</p>
-                                <button class="btn btn-primary" type="submit">Buy</button>
+                                <p className="card-text">The sporty elegence for your bold city lifestyle</p>
+                                <button className="btn btn-primary" type="submit">Buy</button>
                             </div>
                         </div>
                     </div>
-                    <div class="col">
+                    <div className="col">
                         <div className="card h-100" style={{ width: "200px" }}>
-                            <img src="vespa3.png" class="card-img-top" alt="..."/>
+                            <img src="vespa3.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">VESPA LX 125</h5>
                                 <p className="card-text">Some quick example text.</p>
-                                <button class="btn btn-primary" type="submit">Buy</button>
+                                <button className="btn btn-primary" type="submit">Buy</button>
                             </div>
                         </div>
                     </div>
-                    <div class="col">
+                    <div className="col">
                         <div className="card h-100" style={{ width: "200px" }}>
-                            <img src="vespa2.png" class="card-img-top" alt="..."/>
+                            <img src="vespa2.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">VESPA SPRINT 150 ABS</h5>
-                                <p class="card-text">The sporty elegence for your bold city lifestyle</p>
-                                <button class="btn btn-primary" type="submit">Buy</button>
+                                <p className="card-text">The sporty elegence for your bold city lifestyle</p>
+                                <button className="btn btn-primary" type="submit">Buy</button>
                             </div>
                         </div>
                     </div>
-                    <div class="col">
+                    <div className="col">
                         <div className="card h-100" style={{ width: "200px" }}>
-                            <img src="vespa.png" class="card-img-top" alt="..."/>
+                            <img src="vespa.png" className="card-img-top" alt="..." />
                             <div className="card-body">
                                 <h5 className="card-title">VESPA LX 125</h5>
                                 <p className="card-text">Some quick example text.</p>
-                                <button class="btn btn-primary" type="submit">Buy</button>
+                                <button className="btn btn-primary" type="submit">Buy</button>
                             </div>
                         </div>
                     </div>
@@ -196,4 +210,52 @@ let NewCard = () => {
     );
 }
 
+const ProductCard = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/api/type`)
+            .then((response) => {
+                setProducts(response.data.data); // Access nested `data` property
+            })
+            .catch((error) => {
+                console.error("There was an error fetching the data!", error);
+            });
+    }, []);
+
+    return (
+        <div className="container mt-5 mb-5 text-center">
+        <h2 className="mb-4">Product</h2>
+    
+        <div className="row g-4">
+          {Array.isArray(products) && products.length > 0 ? (
+            products.map((data) => (
+              <div key={data.id} className="col-md-4"> {/* Adjust column size as needed */}
+                <div className="card card-sale">
+                  <img
+                    src={`https://res.cloudinary.com/dsfqsjycy/image/upload/${data.asset.path}`}
+                    alt={data.name}
+                    className="card-img-top"
+                  />
+                  <div className="card-body mb-2">
+                    <h5 className="card-title">{data.name}</h5>
+                    <p className="card-price">
+                      Rp{(data.price / 1e6).toFixed(2)} Juta
+                    </p>
+                  </div>
+                  <button className="btn btn-primary m-2" type="button">
+                    Buy
+                  </button>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>Loading products...</p> 
+          )}
+        </div>
+      </div>
+    );
+};
+
+// export default LandingList;
 export default Landing;
--- new-app/src/index.js
+++ new-app/src/index.js
@@ -9,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Landing />
+    {/* <LandingList/> */}
   </React.StrictMode>
 );
 
